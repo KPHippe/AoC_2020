16d4c4b7514932df25c87068d1e7315712b865c1
Kyle Hippe <kyle.hippe@gmail.com>
2020-12-16 22:42:16 -0700

    Python day 17 solve

---
+++ day_17/conway_cubes_4d.py
@@ -0,0 +1,78 @@
+example_input = '''\
+.#.
+..#
+###
+'''
+
+puzzle_input = '''\
+..#....#
+##.#..##
+.###....
+#....#.#
+#.######
+##.#....
+#.......
+.#......'''
+
+from copy import deepcopy
+
+def parse_input(raw_input):
+    pocket_dimension = {}
+    for row, line in enumerate(raw_input.split('\n')):
+        for col, elem in enumerate(list(line)):
+            pocket_dimension[(row, col, 0, 0)] = elem
+
+
+    return pocket_dimension
+
+
+def cycle_dimension(init_dimension, cycles=6):
+    dimension = deepcopy(init_dimension)
+    for i in range(cycles):
+        expanded_dimension = deepcopy(dimension)
+        #add new points in case they don't exist
+        for coords, state in dimension.items():
+            for x, y, z, w in _generate_nearby_points(*coords):
+                if not (x, y, z, w) in expanded_dimension:
+                    expanded_dimension[(x,y,z, w)] = '.'
+
+        for coords, state in expanded_dimension.items():
+            dimension[coords] = det_cube_state(expanded_dimension, *coords)
+
+
+    return len([e for e in dimension.values() if '#' in e])
+
+def det_cube_state(dimension_state, x, y, z, w):
+    surrounding_active = 0
+    for targ_x, targ_y, targ_z, targ_w in _generate_nearby_points(x, y, z, w):
+
+        if (targ_x, targ_y, targ_z, targ_w) in dimension_state and dimension_state[(targ_x, targ_y, targ_z, targ_w)] == '#':
+            surrounding_active += 1
+
+    if dimension_state[(x, y, z, w)] == '#':
+        if surrounding_active >=2 and surrounding_active <=3:
+            return "#"
+        else:
+            return "."
+    elif dimension_state[(x, y, z, w)] == ".":
+        if surrounding_active == 3:
+            return "#" #unhappy cube, wants to be active
+        else:
+            return "."
+
+def _generate_nearby_points(x, y, z, w):
+    for new_x in range(x-1, x+2, 1):
+        for new_y in range(y-1, y+2, 1):
+            for new_z in range(z-1, z+2, 1):
+                for new_w in range(w-1, w+2, 1):
+                    if new_x == x and new_y == y and new_z == z and new_w == w:
+                        continue
+                    yield new_x, new_y, new_z, new_w
+
+if __name__ == '__main__':
+    # init_dimension = parse_input(example_input)
+    init_dimension = parse_input(puzzle_input)
+
+    total_active = cycle_dimension(init_dimension, 6)
+
+    print(f'Total active: {total_active}')
